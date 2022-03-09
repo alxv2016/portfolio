8ef96dec6d21d715838aa413b3b5776d9e8b5a8a
Alex Vong <avong2011@gmail.com>
2022-03-09 08:33:48 -0500

    feat: added links

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,27 @@
-import {Component, HostBinding, OnInit} from '@angular/core';
+import {Component, HostBinding, OnDestroy, OnInit} from '@angular/core';
+import {Subject, takeUntil} from 'rxjs';
+import {ContentService} from 'src/app/services/content.service';
+import {AlxvCollection} from 'src/app/services/models/content.interface';
 
 @Component({
   selector: 'c-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject();
+  siteContent?: AlxvCollection;
   @HostBinding('class') class = 'c-footer';
-  constructor() {}
+  constructor(private contentService: ContentService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.contentService.siteContent$.pipe(takeUntil(this.unsubscribe$)).subscribe((resp) => {
+      this.siteContent = resp;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(0);
+    this.unsubscribe$.complete();
+  }
 }
