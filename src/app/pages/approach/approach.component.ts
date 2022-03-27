96ce4e279359fcad9731eac3971b5fd8123885ef
Alex Vong <avong2011@gmail.com>
2022-03-27 12:02:43 -0400

    feat: adding hero banner to approach page

--- src/app/pages/approach/approach.component.ts
+++ src/app/pages/approach/approach.component.ts
@@ -1,22 +1,39 @@
-import {Component, ElementRef, HostBinding, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostBinding, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Subject, takeUntil} from 'rxjs';
 import {ContentService} from 'src/app/services/content.service';
 import {AlxvCollection} from 'src/app/services/models/content.interface';
+import {gsap} from 'gsap';
 
 @Component({
   selector: 'c-approach',
   templateUrl: './approach.component.html',
   styleUrls: ['./approach.component.scss'],
 })
-export class ApproachComponent implements OnInit, OnDestroy {
+export class ApproachComponent implements OnInit, AfterViewInit, OnDestroy {
   private unsubscribe$ = new Subject();
   siteContent?: AlxvCollection;
   @HostBinding('class') class = 'c-approach';
+  @ViewChild('scrollingHeadline') scrollHeadline!: ElementRef;
   constructor(private contentService: ContentService) {}
 
   ngOnInit(): void {
     this.contentService.siteContent$.pipe(takeUntil(this.unsubscribe$)).subscribe((resp) => {
       this.siteContent = resp;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const scrollingHeadline = this.scrollHeadline.nativeElement;
+    gsap.to(scrollingHeadline, {
+      x: '-50%',
+      duration: 20,
+      ease: 'linear',
+      repeat: -1,
+      // onComplete: () => {
+      //   gsap.set(scrollingHeadline, {
+      //     x: '100%'
+      //   })
+      // }
     });
   }
 
