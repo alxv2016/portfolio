334721e16fbc14ff8912392127f7000013434cfb
Alex Vong <avong2011@gmail.com>
2022-04-02 23:09:46 -0400

    fix: html pipe and blog styles

--- src/app/services/as-html.pipe.ts
+++ src/app/services/as-html.pipe.ts
@@ -7,71 +7,55 @@ import * as prismicH from '@prismicio/helpers';
 })
 export class AsHTMLPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
-  transform(content: any, blog: boolean = false): SafeHtml {
-    if (blog) {
-      const asBlogHTML = prismicH.asHTML<any>(content, null, this.blogHtmlSerializer);
-      if (asBlogHTML) {
-        return this.sanitizer.bypassSecurityTrustHtml(asBlogHTML);
-      }
-    } else {
-      const asHTML = prismicH.asHTML<any>(content, null, this.htmlSerializer);
-      if (asHTML) {
-        return this.sanitizer.bypassSecurityTrustHtml(asHTML);
-      }
+
+  transform(content: any): SafeHtml {
+    const asHTML = prismicH.asHTML<any>(content, null, this.htmlSerializer);
+    if (asHTML) {
+      return this.sanitizer.bypassSecurityTrustHtml(asHTML);
     }
     return 'Missing content';
   }
 
-  private blogHtmlSerializer(type: any, element: any, content: any, children: any) {
-    if (element.type === 'heading1') {
-      return `<h1 class="heading-1">${children}</h1>`;
-    }
-    if (element.type === 'heading2') {
-      return `<h2 class="heading-2">${children}</h2>`;
-    }
-    if (element.type === 'heading3') {
-      return `<h3 class="heading-3">${children}</h3>`;
-    }
-    if (element.type === 'heading4') {
-      return `<h4 class="heading-4">${children}</h4>`;
-    }
-    if (element.type === 'list-item') {
-      return `<li class="ul-list-item">${children}</li>`;
-    }
-    if (element.data?.label === 'code-block') {
-      return `<code>${children}</code>`;
-    }
-    if (element.data?.label === 'code-css') {
-      return `<pre><code class="language-css">${children}</code></pre>`;
-    }
-    if (element.data?.label === 'code-scss') {
-      return `<pre><code class="language-scss">${children}</code></pre>`;
-    }
-    if (element.data?.label === 'code-js') {
-      return `<pre><code class="language-javascript">${children}</code></pre>`;
-    }
-    if (element.data?.label === 'code-ts') {
-      return `<pre><code class="language-typescript">${children}</code></pre>`;
-    }
-    if (element.data?.label === 'iframe') {
-      return `<iframe class="codepen-iframe" scrolling="no" frameborder="no" loading="lazy" allowtransparency="true" src="${children}"></iframe>`;
-    }
-    return null;
-  }
-
   private htmlSerializer(type: any, element: any, content: any, children: any) {
-    if (element.type === 'heading1') {
-      return `<h1>${children}</h1>`;
+    switch (true) {
+      case element.label === 'display1':
+        return `<h1 class="rt-display-1">${children}</h1>`;
+      case element.label === 'display2':
+        return `<h2 class="rt-display-2">${children}</h2>`;
+      case element.label === 'display3':
+        return `<h3 class="rt-display-3">${children}</h3>`;
+      case element.type === 'heading1':
+        return `<h1 class="rt-heading-1">${children}</h1>`;
+      case element.type === 'heading2':
+        return `<h2 class="rt-heading-2">${children}</h2>`;
+      case element.type === 'heading3':
+        return `<h3 class="rt-heading-3">${children}</h3>`;
+      case element.type === 'heading4':
+        return `<h4 class="rt-heading-4">${children}</h4>`;
+      case element.type === 'heading5':
+        return `<h5 class="rt-heading-5">${children}</h5>`;
+      case element.type === 'heading6':
+        return `<h6 class="rt-heading-6">${children}</h6>`;
+      case element.label === 'overline':
+        return `<span class="rt-overline">${children}</span>`;
+      case element.label === 'css':
+        return `<pre class="language-css"><code>${children}</code></pre>`;
+      case element.label === 'scss':
+        return `<pre class="language-scss"><code>${children}</code></pre>`;
+      case element.label === 'html':
+        return `<pre class="language-html"><code>${children}</code></pre>`;
+      case element.label === 'js':
+        return `<pre class="language-js"><code>${children}</code></pre>`;
+      case element.label === 'ts':
+        return `<pre class="language-ts"><code>${children}</code></pre>`;
+      case element.label === 'code':
+        return `<pre class="code"><code>${children}</code></pre>`;
+      case element.label === 'iframe':
+        return `<iframe class="codepen-iframe" scrolling="no" frameborder="no" loading="lazy" allowtransparency="true" src="${children}"></iframe>`;
+      case element.type === 'group-list-item':
+        return `<ul class="rt-list">${children}</ul>`;
+      default:
+        return null;
     }
-    if (element.type === 'heading2') {
-      return `<h2 class="display">${children}</h2>`;
-    }
-    if (element.type === 'heading3') {
-      return `<h3 class="display-2">${children}</h3>`;
-    }
-    if (element.data?.label === 'underline') {
-      return `<span class="underline">${children}</span>`;
-    }
-    return null;
   }
 }
--- src/app/services/prism-highlight.service.ts
+++ src/app/services/prism-highlight.service.ts
@@ -5,6 +5,7 @@ import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-typescript';
+//import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,18 @@ import 'prismjs/components/prism-typescript';
 export class PrismHighlightService {
   highlightAll(): void {
     Prism.manual = true;
+    // console.log(Prism.plugins['NormalizeWhitespace']);
+    // Prism.plugins['NormalizeWhitespace'].setDefaults({
+    //   'remove-trailing': true,
+    //   'remove-indent': true,
+    //   'left-trim': true,
+    //   'right-trim': true,
+    //   'break-lines': 60,
+    //   'spaces-to-tabs': 4,
+    // });
+    Prism.hooks.add('before-highlight', function (env: any) {
+      env.code = env.element.innerText;
+    });
     Prism.highlightAll();
   }
 }
