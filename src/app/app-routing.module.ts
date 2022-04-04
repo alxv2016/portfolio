f45bd53d693d0ce290d54315390532819287086f
Alex Vong <avong2011@gmail.com>
2022-04-04 14:32:39 -0400

    fix: netlify angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     path: 'blog/:id',
     component: BlogComponent,
   },
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
 @NgModule({
