9d08bba75b924a0ccf1b4983fc9d591a89ed5364
Alex Vong <avong2011@gmail.com>
2022-04-19 20:49:30 -0400

    fix: typography styles

--- src/app/pages/design-system/design-system.component.ts
+++ src/app/pages/design-system/design-system.component.ts
@@ -8,6 +8,11 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DesignSystemComponent implements OnInit {
+  tints: string[] = ['Tint 1', 'Tint 2', 'Tint 3', 'Tint 4', 'Tint 5', 'Tint 6'];
+  greys: string[] = ['Grey 1', 'Grey 2', 'Grey 3', 'Grey 4', 'Grey 5', 'Grey 6'];
+  nuetrals: string[] = ['Nuetral 1', 'Nuetral 2', 'Neutral 3', 'Neutral 4', 'Neutral 5', 'Neutral 6'];
+  shades: string[] = ['Shade 1', 'Shade 2', 'Shade 3', 'Shade 4', 'Shade 5', 'Shade 6'];
+  brandColors: string[] = ['Brand', 'Accent 1', 'Accent 2'];
   constructor() {}
 
   ngOnInit(): void {}
