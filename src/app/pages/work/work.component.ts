8eaa732e02c3b5c520b7ddd38cde46908c1b3f54
Alex Vong <avong2011@gmail.com>
2022-04-21 08:39:55 -0400

    feat: added process off canvas logic

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -4,6 +4,7 @@ import {
   ChangeDetectorRef,
   Component,
   ElementRef,
+  Injector,
   OnInit,
   QueryList,
   ViewChildren,
@@ -11,6 +12,9 @@ import {
 } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable, switchMap, tap} from 'rxjs';
+import {AppComponent} from 'src/app/app.component';
+import {ColorSchemeComponent} from 'src/app/components/color-scheme/color-scheme.component';
+import {OffCanvasService} from 'src/app/components/off-canvas/off-canvas.service';
 import {PrismicResult} from 'src/app/services/models/prismic.interface';
 import {PrismService} from 'src/app/services/prism.service';
 import {PrismicService} from 'src/app/services/prismic.service';
@@ -28,10 +32,12 @@ export class WorkComponent implements OnInit, AfterViewInit {
   @ViewChildren('htmlContent', {read: ElementRef}) htmlContent!: QueryList<ElementRef>;
   constructor(
     private route: ActivatedRoute,
+    private offCanvas: OffCanvasService,
     private prismic: PrismicService,
     private cd: ChangeDetectorRef,
     private element: ElementRef,
-    private prism: PrismService
+    private prism: PrismService,
+    private inject: Injector
   ) {}
 
   ngOnInit(): void {
@@ -46,10 +52,16 @@ export class WorkComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+    const parent = this.inject.get<AppComponent>(AppComponent);
+    this.offCanvas.getViewRef(parent.offCanvasHost.viewContainerRef);
     this.htmlContent.changes.subscribe((_) => {
       //
       console.log(_);
       this.prism.highlightAll();
     });
+  }
+
+  openOffCanvas(): void {
+    this.offCanvas.createOffCanvas(ColorSchemeComponent);
   }
 }
